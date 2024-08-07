3755d4c1d5b3c70b52aa0a6511f61e35a1463eb3
Jay <jay.zhangjunjie@outlook.com>
2024-08-07 11:07:17 +0800

    📃 docs(all): updata doc
    updata py doc and readme

--- cv/cam_params.py
+++ cv/cam_params.py
@@ -10,32 +10,29 @@ if platform.system() != "Linux":
     exit()
 
 
-
 class V4l2ToCv2(Enum):
-    BRIGHTNESS                  = cv2.CAP_PROP_BRIGHTNESS
-    CONTRAST                    = cv2.CAP_PROP_CONTRAST
-    SATURATION                  = cv2.CAP_PROP_SATURATION
-    HUE                         = cv2.CAP_PROP_HUE
-    AUTO_WHITE_BALANCE          = cv2.CAP_PROP_AUTO_WB
-    DO_WHITE_BALANCE            = None
-    RED_BALANCE                 = cv2.CAP_PROP_WHITE_BALANCE_RED_V
-    BLUE_BALANCE                = cv2.CAP_PROP_WHITE_BALANCE_BLUE_U
-    GAMMA                       = cv2.CAP_PROP_GAMMA
-    WHITENESS                   = None
-    EXPOSURE                    = cv2.CAP_PROP_EXPOSURE
-    AUTOGAIN                    = None
-    GAIN                        = cv2.CAP_PROP_GAIN
-    WHITE_BALANCE_TEMPERATURE   = cv2.CAP_PROP_WB_TEMPERATURE
+    BRIGHTNESS = cv2.CAP_PROP_BRIGHTNESS
+    CONTRAST = cv2.CAP_PROP_CONTRAST
+    SATURATION = cv2.CAP_PROP_SATURATION
+    HUE = cv2.CAP_PROP_HUE
+    AUTO_WHITE_BALANCE = cv2.CAP_PROP_AUTO_WB
+    DO_WHITE_BALANCE = None
+    RED_BALANCE = cv2.CAP_PROP_WHITE_BALANCE_RED_V
+    BLUE_BALANCE = cv2.CAP_PROP_WHITE_BALANCE_BLUE_U
+    GAMMA = cv2.CAP_PROP_GAMMA
+    WHITENESS = None
+    EXPOSURE = cv2.CAP_PROP_EXPOSURE
+    AUTOGAIN = None
+    GAIN = cv2.CAP_PROP_GAIN
+    WHITE_BALANCE_TEMPERATURE = cv2.CAP_PROP_WB_TEMPERATURE
     WHITE_BALANCE_TEMPERATURE_AUTO = cv2.CAP_PROP_AUTO_WB
-    SHARPNESS                   = cv2.CAP_PROP_SHARPNESS
-    BACKLIGHT_COMPENSATION      = cv2.CAP_PROP_BACKLIGHT
-    AUTOBRIGHTNESS              = None
+    SHARPNESS = cv2.CAP_PROP_SHARPNESS
+    BACKLIGHT_COMPENSATION = cv2.CAP_PROP_BACKLIGHT
+    AUTOBRIGHTNESS = None
     EXPOSURE_AUTO = cv2.CAP_PROP_AUTO_EXPOSURE
     EXPOSURE_ABSOLUTE = cv2.CAP_PROP_EXPOSURE
     FOCUS_ABSOLUTE = cv2.CAP_PROP_FOCUS
     FOCUS_AUTO = cv2.CAP_PROP_AUTOFOCUS
-
-
 
 
 @dataclass
@@ -90,8 +87,8 @@ class BoolParam:
                 if p != "":
                     k, v = p.split("=")
                     params.update({k: bool(v)})
-            params.update({"min":0})
-            params.update({"max":1})
+            params.update({"min": 0})
+            params.update({"max": 1})
             return params
 
         name = s[: s.find(" ")]
@@ -134,7 +131,7 @@ class MenuParam:
 
 
 class CameraParams:
-    WINDOWS_NAME = 'Camera Settings'
+    WINDOWS_NAME = "Camera Settings"
 
     def __init__(self, camIndex) -> None:
 
@@ -142,7 +139,7 @@ class CameraParams:
         self.params = {}
         self.__getCtrlsListInfo()
         cv2.namedWindow(self.WINDOWS_NAME, cv2.WINDOW_AUTOSIZE)
-        self.cap = cv2.VideoCapture(0,  cv2.CAP_V4L2)
+        self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
         self.createTrackbar()
 
     def __getCtrlsListInfo(self):
@@ -168,26 +165,33 @@ class CameraParams:
                 k, v = MenuParam.parse(line)
                 self.params.update({k: v})
 
-
     def createTrackbar(self):
         for k in self.params.keys():
             # if type(params[k]) == IntParam :
-                cv2.createTrackbar(k, self.WINDOWS_NAME, self.params[k].value, self.params[k].max, self.updateParam)
+            cv2.createTrackbar(
+                k,
+                self.WINDOWS_NAME,
+                self.params[k].value,
+                self.params[k].max,
+                self.updateParam,
+            )
 
-
-    
     def updateParam(self, arg):
         for k in self.params.keys():
-            k:str
+            k: str
             v = cv2.getTrackbarPos(k, self.WINDOWS_NAME)
             if v != self.params[k].value:
-                if k.upper() in V4l2ToCv2.__members__.keys() and V4l2ToCv2[k.upper()].value is not None :
+                if (
+                    k.upper() in V4l2ToCv2.__members__.keys()
+                    and V4l2ToCv2[k.upper()].value is not None
+                ):
                     self.params[k].value = v
                     print(f"设置相机参数 | {k}:{v} | default:{self.params[k].default}")
                     self.cap.set(V4l2ToCv2[k.upper()].value, v)
                 else:
-                    print(f"未能正确找到设置该参数的接口 | {k}:{v} | default:{self.params[k].default}")
-
+                    print(
+                        f"未能正确找到设置该参数的接口 | {k}:{v} | default:{self.params[k].default}"
+                    )
 
     def show(self):
         while True:
@@ -196,16 +200,19 @@ class CameraParams:
             if not ret:
                 print("Error: Could not read frame.")
                 break
-            
+
             # 显示帧
-            cv2.imshow('Camera', frame)
-            
+            cv2.imshow("Camera", frame)
+
             # 按下q键退出循环
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         # 释放资源
         self.cap.release()
         cv2.destroyAllWindows()
-        
-a = CameraParams(0)
-a.show()
+
+
+if __name__ == "__main__":
+
+    a = CameraParams(0)
+    a.show()

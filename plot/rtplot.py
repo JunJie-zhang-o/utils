d5a3ba8b332b875d9164b4587ff0d0b7efa8aced
Jay <jay.zhangjunjie@outlook.com>
2024-08-22 09:59:59 +0800

    ✨ feat(rtplot.py): 优化显示效果,增加数据保存

--- plot/rtplot.py
+++ plot/rtplot.py
@@ -1,29 +1,22 @@
-from re import X
 import sys
-from threading import Event, Thread
 import time
-from typing import Callable
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from datetime import datetime
+from threading import Event, Thread
+from turtle import color
 
-import pyqtgraph as pg
 import numpy as np
+import pyqtgraph as pg
 import zmq
+from PyQt5.QtGui import QPen
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from pyqtgraph.Qt import QtGui
 
 # 禁用科学记数法
 np.set_printoptions(suppress=True)
 
 class Suber(Thread):
-    """
-        基于zmq PUB的SUB线程实现
-    """
-    def __init__(self, address: str, topic: str=""):
-        """
-            订阅zmq 的 PUB 发送的数据
 
-        Args:
-            address (str): 订阅地址. 例如:  tcp://192.168.40.241:5556
-            topic (str, optional): 如果PUB指定那么SUB也需要指定. Defaults to "".
-        """
+    def __init__(self, address, topic=""):
         super().__init__(name=f"Suber Thread | address:{address}, topic:{topic}", daemon=True)
         self.address = address
         self.topic = topic
@@ -54,9 +47,6 @@ class Suber(Thread):
 
 
     def stop(self):
-        """
-            停止SUB的后台线程
-        """
         self._stop_event.set()
         self.socket.close()
         self.context.term()
@@ -65,30 +55,31 @@ class Suber(Thread):
 
 
 class PlotSubWindow:
-    """
-        实时绘制子窗口类
-    """
-    def __init__(self, title: str, callback: Callable, row: int, col: int, yRange: tuple, win:pg.GraphicsLayoutWidget):
-        """
-        _summary_
-
-        Args:
-            title (str): 窗口title
-            callback (Callable): 获取数据回调函数,该回调函数在每个时间周期内调用,并且该回调应该返回该子窗口上x和y对应的数据
-            row (int): 在主窗口上的行位置
-            col (int): 在主窗口上的列位置
-            yRange (tuple, optional): 初始Y轴的区间. Defaults to None.
-            win (pg.GraphicsLayoutWidget): 子窗口要添加到的主窗口
-        """
+    def __init__(self, title, callback, row, col, yRange, win):
         self.plot = win.addPlot(row, col, title=title)
-        self.plot.setTitle(title, size="20pt")
+        self.plot.setTitle(title, size="30pt")
         self.x_data = []
         self.y_data = []
+
         self.curve = self.plot.plot()
+        self.curve.setPen(pg.mkPen(color="w", width=3))
+
+
+        # 设置坐标轴标签的字体大小
+        axis_font = QtGui.QFont()
+        axis_font.setPointSize(16)  # 增加坐标轴字体大小
+        axis_font.setBold(True)
+        self.plot.getAxis('bottom').setStyle(tickFont=axis_font)
+        self.plot.getAxis('bottom').setPen(pg.mkPen(color="w", width=2))
+        self.plot.getAxis('left').setPen(pg.mkPen(color="w", width=2))
+        self.plot.getAxis('left').setStyle(tickFont=axis_font)
+
 
         # 添加显示最大值和最小值的文本项
-        self.max_text = pg.TextItem(anchor=(1, 1))
-        self.min_text = pg.TextItem(anchor=(1, 0))
+        self.max_text = pg.TextItem(anchor=(1, 1), color=(100,100,100))
+        self.min_text = pg.TextItem(anchor=(1, 0), color=(100,100,100))
+        self.max_text.setFont(axis_font)
+        self.min_text.setFont(axis_font)
         self.plot.addItem(self.max_text)
         self.plot.addItem(self.min_text)
 
@@ -99,9 +90,6 @@ class PlotSubWindow:
         self.callback = callback
 
     def update(self):
-        """
-            数据更新
-        """
         x, y = self.callback()
 
         if x is None or y is None:
@@ -123,111 +111,90 @@ class PlotSubWindow:
             self.min_text.setPos(self.x_data[-1], min_y)
 
             # 更新y轴的范围
-            self.plot.setYRange(min_y, max_y)
+            # self.plot.setYRange(min_y, max_y)
+            self.plot.setYRange(-10, 10)
 
+
+    def save_data(self, filename):
+        """保存当前窗口的数据为CSV文件"""
+        data = np.column_stack((self.x_data, self.y_data))
+        np.savetxt(filename, data, delimiter=",", header="Timestamp,Value", comments="")
+        print(f"Data saved to {filename}")
 
 
 
 class RealTimePlot(QMainWindow):
-    """
-        实时接收数据plot
-    """
-    def __init__(self,title:str, msec:int):
-        """
-        Args:
-            title (str): 窗口的名册很难过
-            msec (int): 数据刷新频率,单位:ms
-        """
+    
+    def __init__(self,title, msec):
         super().__init__()
         self.win = pg.GraphicsLayoutWidget(show=True)
         self.setWindowTitle(title)
         self.setCentralWidget(self.win)
         self._plots = {}
 
-
+        # 在窗口显示时最大化
+        self.showMaximized()
         # 设置一个计时器来调用更新函数
         self.timer = pg.QtCore.QTimer()
         self.timer.start(msec)  # 每100毫秒更新一次
 
 
-    def addSubWindow(self, title: str, callback:Callable, row: int, col: int,  yRange: tuple=None) -> PlotSubWindow:
-        """
-            添加一个子窗口
-
-        Args:
-            title (str): 窗口title
-            callback (Callable): 获取数据回调函数,该回调函数在每个时间周期内调用,并且该回调应该返回该子窗口上x和y对应的数据
-            row (int): 在主窗口上的行位置
-            col (int): 在主窗口上的列位置
-            yRange (tuple, optional): 初始Y轴的区间. Defaults to None.
-
-        Returns:
-            PlotSubWindow: 创建的子窗口
-        """
+    def addSubWindow(self, title, callback, row, col,  yRange=None):
         subWindow = self.createSubWindow(title, callback, row, col, yRange)
         self._plots.update({title: subWindow})
         self.timer.timeout.connect(subWindow.update)
         return subWindow
         
 
-    def delSubWindow(self, title: str) -> None:
-        """
-            删除子窗口
-
-        Args:
-            title (str): 要删除的子窗口名称
-        """
+    def delSubWindow(self, title):
         if title in self._plots.keys():
             self._plots.pop(title)
     
 
-    def createSubWindow(self, title: str, callback: Callable, row: int, col: int, yRange: tuple=None) -> PlotSubWindow:
-        """
-        创建一个子窗口
-
-        Args:
-            title (str): 窗口name
-            callback (Callable): 回调
-            row (int): 行号
-            col (int): 列号
-            yRange (tuple, optional): 设置y轴的初始区间. Defaults to None.
-
-        Returns:
-            PlotSubWindow: 子窗口
-        """
-        subWindow = PlotSubWindow(title, callback,  row, col, yRange, self.win)
-        return subWindow
+    def createSubWindow(self, title, callback, row, col, yRange=None):
+       subWindow = PlotSubWindow(title, callback,  row, col, yRange, self.win)
+       return subWindow
 
 
-    def getSubWindow(self, title: str):
-        """
-        _summary_
-
-        Args:
-            title (str): _description_
-
-        Returns:
-            _type_: _description_
-        """
+    def getSubWindow(self, title):
         return self._plots.get(title, None)
+
+    def save_all_data(self):
+        """保存所有窗口的数据"""
+        formatTime = datetime.now().strftime("%Y%m%d_%H_%M_%S")
+        for title, plot in self._plots.items():
+            filename = f"{formatTime}_{title}_data.csv"
+            plot.save_data(filename)
 
     
 # TODO:增加数据保存
 
 
 if __name__ == '__main__':
+    # suber = Suber("tcp://192.168.1.7:5557")
     # suber = Suber("tcp://192.168.1.2:5556")
-    suber = Suber("tcp://192.168.40.241:5556")
-    # suber = Suber("tcp://127.0.0.1:5556")
+    # suber = Suber("tcp://192.168.40.241:5556")
+    suber = Suber("tcp://127.0.0.1:5555")
     suber.start()
     app = QApplication(sys.argv)
+    # ex = RealTimePlot((-15, 15), callback=lambda: (float(suber.timestamp), float(suber.message.split(",")[0])))
+    # ex.show()
+    # exit = app.exec_()
+    # # values = np.column_stack(ex.x_data, ex.y_data)
+    # # np.savetxt("values.txt",values)
+    # sys.exit(exit)
 
-
-    rtPlot = RealTimePlot("数据", 100)
+    rtPlot = RealTimePlot("Data", 100)
     rtPlot.addSubWindow("X", lambda: (float(suber.timestamp), float(suber.message.split(",")[0])), 1,1)
     rtPlot.addSubWindow("Y", lambda: (float(suber.timestamp), float(suber.message.split(",")[1])), 2,1)
     rtPlot.addSubWindow("Z", lambda: (float(suber.timestamp), float(suber.message.split(",")[2])), 3,1)
     # rtPlot.addSubWindow("t3", lambda:(round(time.time(), 6),3), 2,1)
     # rtPlot.addSubWindow("t4", lambda:(round(time.time(), 6),4), 2,2)
+
+    # 退出时自动保存数据
+    def on_exit():
+        rtPlot.save_all_data()
+        suber.stop()
+    app.aboutToQuit.connect(lambda: rtPlot.save_all_data())
     rtPlot.show()
     sys.exit(app.exec_())
